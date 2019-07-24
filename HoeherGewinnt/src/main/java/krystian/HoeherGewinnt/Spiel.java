34a5e2fbe29a1a7fa24d323ed16b140fc673da98
KrystianMyszk <45992445+KrystianMyszk@users.noreply.github.com>
2019-07-24 20:14:21 +0200

    Spiel erweitert

--- HoeherGewinnt/src/main/java/krystian/HoeherGewinnt/Spiel.java
+++ HoeherGewinnt/src/main/java/krystian/HoeherGewinnt/Spiel.java
@@ -7,5 +7,35 @@ public class Spiel {
 	private Ablagestapel ablagestapel;
 	private List<Spieler> spieler;
 	
+	
+	public Spiel(Ablagestapel ablagestapel, List<Spieler> spieler) {
+		super();
+		this.ablagestapel = ablagestapel;
+		this.spieler = spieler;
+	}
+
+
+	public Ablagestapel getAblagestapel() {
+		return ablagestapel;
+	}
+
+
+	public void setAblagestapel(Ablagestapel ablagestapel) {
+		this.ablagestapel = ablagestapel;
+	}
+
+
+	public List<Spieler> getSpieler() {
+		return spieler;
+	}
+
+
+	public void setSpieler(List<Spieler> spieler) {
+		this.spieler = spieler;
+	}
+	
+	
+	
+	
 
 }
